3b6df54a3266bea26ca17380329bbbbc837384f5
AnthonyBuncio <anthonyb.ft@gmail.com>
2017-05-17 12:40:39 -0500

    original contact form

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php 
-$emailTo = 'yourmail@example.com';
-$siteTitle = 'YourSiteTitle';
+$emailTo = 'anthonyb.ft@gmail.com';
+$siteTitle = 'Portfolio Website';
 
 error_reporting(E_ALL ^ E_NOTICE); // hide all basic notices from PHP
 
